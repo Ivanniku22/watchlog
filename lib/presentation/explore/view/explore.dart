b2ac2feff5fc0cbe7cb46794dd5b9e9e405b99c2
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2026-07-22 22:17:52 +0530

    New search box added

--- lib/presentation/explore/view/explore.dart
+++ lib/presentation/explore/view/explore.dart
@@ -6,12 +6,41 @@ class Explore extends GetView<ExploreController>{
 
   const Explore({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Explore',
-        style: Theme.of(context).textTheme.bodyMedium
+
+    final List<String> filters = [
+      "FEED",
+      "DISCOVER",
+      "GROUPS",
+      "ACTIVITY"
+    ];
+
+
+    return SingleChildScrollView(
+      physics: BouncingScrollPhysics(),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+          child: Column(
+            children: [
+              TextField(
+                style: Theme.of(context).textTheme.bodySmall,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 15),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey
+                    )
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
