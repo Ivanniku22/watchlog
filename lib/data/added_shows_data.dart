7d4bb1ab1e83fcc52dc9d803bf28fe479aae5d3c
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2026-07-23 23:30:02 +0530

    Most Added Shows added

---
+++ lib/data/added_shows_data.dart
@@ -0,0 +1,35 @@
+import 'models/movie_watchlist_model.dart';
+
+final List<MovieWatchList> addedShows = [
+
+  MovieWatchList(
+      id: 1,
+      poster: "https://media.themoviedb.org/t/p/original/qQclTgLMDvGBuUBFGHRipxkEwWR.jpg"
+  ),
+
+  MovieWatchList(
+      id: 2,
+      poster: "https://media.themoviedb.org/t/p/original/yQvGrMoipbRoddT0ZR8tPoR7NfX.jpg"
+  ),
+
+  MovieWatchList(
+      id: 3,
+      poster: "https://media.themoviedb.org/t/p/original/mGWOmj2jHFol3kOGNv1EhbSDDE1.jpg"
+  ),
+
+  MovieWatchList(
+      id: 4,
+      poster: "https://media.themoviedb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+  ),
+
+  MovieWatchList(
+      id: 5,
+      poster: "https://media.themoviedb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg"
+  ),
+
+  MovieWatchList(
+      id: 6,
+      poster: "https://media.themoviedb.org/t/p/original/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+  ),
+
+];
--- lib/presentation/explore/view/explore.dart
+++ lib/presentation/explore/view/explore.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:watchlog/data/added_shows_data.dart';
 import 'package:watchlog/presentation/explore/controller/explore_controller.dart';
 import 'package:watchlog/presentation/explore/widgets/filters.dart';
 
@@ -64,7 +65,7 @@ class Explore extends GetView<ExploreController>{
 
               SizedBox(height: 10,),
 
-              //Trending Shows Grid view
+             // Trending Shows Grid
               GridView.builder(
                 shrinkWrap: true,
                 physics: NeverScrollableScrollPhysics(),
@@ -78,15 +79,173 @@ class Explore extends GetView<ExploreController>{
                 itemBuilder: (context, index){
                   return ClipRRect(
                     borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      trendingShows[index].poster,
-                      fit: BoxFit.cover,
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: Image.network(
+                            trendingShows[index].poster,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+
+                        Positioned(
+                          top: 8,
+                          right: 8,
+                          child: GestureDetector(
+                            onTap: () {
+                              // TODO: Add show
+                            },
+                            child: Container(
+                              width: 28,
+                              height: 28,
+                              decoration: BoxDecoration(
+                                color: Colors.black.withValues(alpha: 0.25),
+                                borderRadius: BorderRadius.circular(6),
+                                border: Border.all(
+                                  color: const Color(0xFFFFD700),
+                                )
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                color: const Color(0xFFFFD700),
+                                size: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   );
                 },
               ),
 
+              //See more button
+              GestureDetector(
+                onTap: () {
+                  // TODO: Handle tap
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withValues(alpha: 0.25),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    'See More',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              ),
+
+
+              SizedBox(height: 20,),
+
+              // Recommendations 2
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardTheme.color,
+                  borderRadius: BorderRadius.circular(14),
+                  border: Border.all(
+                    color: Colors.grey.shade900,
+                    width: 1,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+                  child: Text(
+                    'Most Added Show',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ),
+              ),
+
               SizedBox(height: 10,),
+
+              //Added Shows Grid
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: addedShows.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 3,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: 0.8,
+                ),
+                itemBuilder: (context, index){
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                          child: Image.network(
+                            addedShows[index].poster,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+
+                        Positioned(
+                          top: 8,
+                          right: 8,
+                          child: GestureDetector(
+                            onTap: () {
+                              // TODO: Add show
+                            },
+                            child: Container(
+                              width: 28,
+                              height: 28,
+                              decoration: BoxDecoration(
+                                  color: Colors.black.withValues(alpha: 0.25),
+                                  borderRadius: BorderRadius.circular(6),
+                                  border: Border.all(
+                                    color: const Color(0xFFFFD700),
+                                  )
+                              ),
+                              child: const Icon(
+                                Icons.add,
+                                color: const Color(0xFFFFD700),
+                                size: 18,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+
+              //See more button
+              GestureDetector(
+                onTap: () {
+                  // TODO: Handle tap
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withValues(alpha: 0.25),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    'See More',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 30,),
             ],
           ),
         ),
