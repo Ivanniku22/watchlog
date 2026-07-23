2e9459b603d63feff3dbd5f5c20afde21668cbad
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2026-07-23 23:02:29 +0530

    Trending Shows added

---
+++ lib/data/trending_shows_data.dart
@@ -0,0 +1,35 @@
+import 'models/movie_watchlist_model.dart';
+
+final List<MovieWatchList> trendingShows = [
+
+  MovieWatchList(
+      id: 1,
+      poster: "https://media.themoviedb.org/t/p/original/5rhTDKUhPYvpdQIijFIs5VoWsON.jpg"
+  ),
+
+  MovieWatchList(
+      id: 2,
+      poster: "https://media.themoviedb.org/t/p/original/6Q21yptoOCUq4ErwVncesLPVplb.jpg"
+  ),
+
+  MovieWatchList(
+      id: 3,
+      poster: "https://media.themoviedb.org/t/p/original/oRuyGUHdoaQxWP3SDfafGkStxTC.jpg"
+  ),
+
+  MovieWatchList(
+      id: 4,
+      poster: "https://media.themoviedb.org/t/p/original/cWXtJhrlruF8CeYuaBGE8vdj3Q9.jpg"
+  ),
+
+  MovieWatchList(
+      id: 5,
+      poster: "https://media.themoviedb.org/t/p/original/zm0KAbOjlt9eR5y7vDiL2dEOwMl.jpg"
+  ),
+
+  MovieWatchList(
+      id: 6,
+      poster: "https://media.themoviedb.org/t/p/original/bRBeSHfGHwkEpImlhxPmOcUsaeg.jpg"
+  ),
+
+];
--- lib/presentation/explore/view/explore.dart
+++ lib/presentation/explore/view/explore.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:watchlog/presentation/explore/controller/explore_controller.dart';
 import 'package:watchlog/presentation/explore/widgets/filters.dart';
 
+import '../../../data/trending_shows_data.dart';
+
 class Explore extends GetView<ExploreController>{
 
   const Explore({super.key});
@@ -37,6 +39,54 @@ class Explore extends GetView<ExploreController>{
 
               //Filters
               Filters(),
+
+              SizedBox(height: 15,),
+
+              // Recommendations
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardTheme.color,
+                  borderRadius: BorderRadius.circular(14),
+                  border: Border.all(
+                    color: Colors.grey.shade900,
+                    width: 1,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 3.0),
+                  child: Text(
+                    'Trending Shows',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ),
+              ),
+
+
+              SizedBox(height: 10,),
+
+              //Trending Shows Grid view
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: trendingShows.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 3,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: 0.8,
+                ),
+                itemBuilder: (context, index){
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      trendingShows[index].poster,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
+
+              SizedBox(height: 10,),
             ],
           ),
         ),
